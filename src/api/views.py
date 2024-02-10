5a2719c6ed078fb17e797604d52f0574fc856dbf
ykulyk404 <evgeniy.kulyk@uipath.com>
2024-02-10 16:30:01 +0200

    probes

--- src/api/views.py
+++ src/api/views.py
@@ -4,6 +4,11 @@ from rest_framework import permissions, viewsets
 from api.serializers import TodoListSerializer, TodoSerializer, UserSerializer
 from lists.models import Todo, TodoList
 
+from django.http import HttpResponse
+from django.utils import timezone
+import time
+
+startup_time = timezone.now()
 
 class IsCreatorOrReadOnly(permissions.BasePermission):
     """
@@ -54,3 +59,18 @@ class TodoViewSet(viewsets.ModelViewSet):
         user = self.request.user
         creator = user if user.is_authenticated else None
         serializer.save(creator=creator)
+
+# # Health Check View
+# def health(request):
+#     return HttpResponse("Health OK", content_type="text/plain")
+
+# # Readiness Check View
+# def ready(request):
+#     # Calculate elapsed time since startup
+#     elapsed_time = timezone.now() - startup_time
+#     if elapsed_time.total_seconds() < 30:
+#         # Return HTTP 500 for the first 30 seconds after startup
+#         return HttpResponse("Service not ready", status=500, content_type="text/plain")
+#     else:
+#         # After 30 seconds, return HTTP 200
+#         return HttpResponse("Readiness OK", content_type="text/plain")
